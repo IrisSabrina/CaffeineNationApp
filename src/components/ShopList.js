9dc593b80c7b1986c05870caa1d15f39c240efe4
ccosat1isd <ccosat1isd@ccosat08isd.local>
2019-06-12 00:14:45 -0600

    Finalized styling

--- src/components/ShopList.js
+++ src/components/ShopList.js
@@ -8,7 +8,7 @@ class ShopList extends Component {
   render() {
     return (
       <div className="shop-list">
-        <h1>Hit up these places when you're in town!</h1>
+        <h1>List your favorite spots below!</h1>
         {this.props.shopsArray.map((shop, index) => {
           return(
             <Shop
